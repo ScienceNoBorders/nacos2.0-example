3f0f68e5c142205c86691a64b32dafe2c553ce6f
XiaoTiJun <971485051@qq.com>
2021-03-30 17:39:36 +0800

    first commit

---
+++ consumer/src/test/java/com/xiaotijun/nacos/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaotijun.nacos.consumer;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ provider/src/main/java/com/xiaotijun/provider/ProviderApplication.java
@@ -0,0 +1,27 @@
+package com.xiaotijun.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+
+    @RestController
+    class EchoController {
+        @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+        public String echo(@PathVariable String string) {
+            return "Hello Nacos Discovery " + string;
+        }
+    }
+
+}
